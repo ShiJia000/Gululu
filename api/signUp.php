39333e88694ec8753aa15b52ba837258b89e5264
jia <1023187342@qq.com>
2019-12-05 10:39:41 -0500

    signUp Check null

--- api/signUp.php
+++ api/signUp.php
@@ -6,23 +6,27 @@
  */
 require_once 'api.php';
 class signUp extends api {
+
 	public function doExecute() {
 		$conn = $this->conn;
 
 		// in case of sql injection
-		$firstname = $this->check($_POST['firstname']);
-		$lastname = $this->check($_POST['lastname']);
-		$userPwd = md5($this->check($_POST['userPwd']));
-		$state = $this->check($_POST['state']);
-		$city = $this->check($_POST['city']);
-		$zipcode = intval($_POST['zipcode']);
-		$address = $this->check($_POST['address']);
-		$phoneNum = intval($_POST['phoneNum']);
-		$email = $this->check($_POST['email']);
+		$this->firstname = $this->check($_POST['firstname']);
+		$this->lastname = $this->check($_POST['lastname']);
+		$this->userPwd = md5($this->check($_POST['userPwd']));
+		$this->state = $this->check($_POST['state']);
+		$this->city = $this->check($_POST['city']);
+		$this->zipcode = intval($_POST['zipcode']);
+		$this->address = $this->check($_POST['address']);
+		$this->phoneNum = intval($_POST['phoneNum']);
+		$this->email = $this->check($_POST['email']);
+
+		// check not null
+		$this->checkNotNull();
 
 		// check if the email is unique;
-		$checkEmail = "SELECT email FROM user WHERE email = '" . $email . "';";
-		$query = mysqli_query($this->conn, $checkEmail);
+		$checkEmail = "SELECT email FROM user WHERE email = '" . $this->email . "';";
+		$query = $conn->query($checkEmail);
 		$data = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
 		if (count($data) !== 0) {
@@ -32,15 +36,15 @@ class signUp extends api {
 		// insert data to user table
 		$sql = "INSERT INTO USER (`firstname`, `lastname`, `user_pwd`, `state`, `city`,
 				`zipcode`, `address`, `phone_num`, `email`) VALUES ( '" 
-	    	. $firstname . "', '" 
-	    	. $lastname . "', '"
-	    	. $userPwd . "', '"
-	    	. $state . "', '"
-	    	. $city . "', "
-	    	. $zipcode . ", '"
-	    	. $address . "', "
-	    	. $phoneNum . ", '"
-	    	. $email . "');";
+	    	. $this->firstname . "', '" 
+	    	. $this->lastname . "', '"
+	    	. $this->userPwd . "', '"
+	    	. $this->state . "', '"
+	    	. $this->city . "', "
+	    	. $this->zipcode . ", '"
+	    	. $this->address . "', "
+	    	. $this->phoneNum . ", '"
+	    	. $this->email . "');";
 
 	    $data = $conn->query($sql);
 
@@ -48,6 +52,40 @@ class signUp extends api {
 			return $data;
 		} else {
 			throw new Exception("An error Occurred!");
+		}
+	}
+
+	/**
+	 * [checkNotNull description]
+	 * @return [type] [void]
+	 */
+	public function checkNotNull() {
+		if (!$this->firstname) {
+			throw new Exception("Firstname cannot be null!");
+		}
+		if (!$this->lastname) {
+			throw new Exception("Firstname cannot be null!");
+		}
+		if (!$this->userPwd) {
+			throw new Exception("Password cannot be null!");
+		}
+		if (!$this->state) {
+			throw new Exception("State cannot be null!");
+		}
+		if (!$this->city) {
+			throw new Exception("City cannot be null!");
+		}
+		if (!$this->zipcode) {
+			throw new Exception("Zipcode cannot be null!");
+		}
+		if (!$this->address) {
+			throw new Exception("Address cannot be null!");
+		}
+		if (!$this->phoneNum) {
+			throw new Exception("Phone num cannot be null!");
+		}
+		if (!$this->email) {
+			throw new Exception("Email cannot be null!");
 		}
 	}
 
