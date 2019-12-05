d706c31301d561069d5c36d414d09a96105ccabe
fthongstar <haochz3@uci.edu>
2019-12-05 16:12:17 -0500

    add unread info

--- api/getReplyMessage.php
+++ api/getReplyMessage.php
@@ -7,13 +7,29 @@ class getReplyMessage extends api {
 	public function doExecute() {
 		// in case of sql injection
 		$mid=intval($_GET['mid']);
+		$isUnRead = intval($_GET['unread']);
 		$replyMessage='';
 
-
-		$replyMessage='SELECT u.firstname, u.lastname, u.photo, r.*
-						FROM reply r, user u
-						WHERE u.uid=r.uid
-						AND r.mid= ' .$mid . ';';
+		if ($isUnRead===1){
+			//unread replys
+			$replyMessage='SELECT u.firstname, u.lastname, u.photo, r.*
+							FROM receive_reply rr, reply r, user u, message m
+							WHERE u.uid=r.uid
+							AND m.mid=r.mid
+							AND m.uid=rr.uid
+							AND rr.rid=r.rid
+							AND is_read=0
+							AND r.mid= ' .$mid . ';';
+		} else{
+			//all replys
+			$replyMessage='SELECT u.firstname, u.lastname, u.photo, r.*
+							FROM receive_reply rr, reply r, user u, message m
+							WHERE u.uid=r.uid
+							AND m.mid=r.mid
+							AND m.uid=rr.uid
+							AND rr.rid=r.rid
+							AND r.mid= ' .$mid . ';';
+		}
 
 		
 		$query = mysqli_query($this->conn, $replyMessage);
