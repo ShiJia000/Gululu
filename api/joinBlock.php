5a8877e83d636f4afb016ed4f7fded367eeb5c92
fthongstar <haochz3@uci.edu>
2019-12-05 20:56:26 -0500

    add join block

---
+++ api/joinBlock.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * http://localhost/db_nextdoor/api/joinBlock
+ */
+require_once 'api.php';
+class joinBlock extends api {
+	public function doExecute() {
+		// in case of sql injection
+		// $uid=intval($_GET['uid']);
+		// $bid=intval($_GET['bid']);
+
+		$conn = $this->conn;
+
+		// in case of sql injection
+
+		$this->uid = intval($_POST['uid']);
+		$this->bid = intval($_POST['bid']);
+
+		//check if the block is empty
+		$check_empty_block='SELECT count(*) as cn
+		FROM join_block
+		WHERE is_approved<>-1
+		AND bid=' . $this->bid  . ';';
+
+		$query = $conn->query($check_empty_block);
+		$data_block = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+		print_r($data_block);
+
+		//check if the user in the block or in other blocks
+		$check_user_in_block='SELECT count(*) as cn
+		FROM join_block
+		WHERE is_approved<>-1
+		AND uid=' . $this->uid  . ';';
+
+		$query = $conn->query($check_user_in_block);
+		$data_user = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+		print_r($data_user);
+
+
+
+		if ($data_block[0]["cn"] === "0" && !$data_user[0]["cn"]){
+			//empty block and new user
+
+			$j_block="INSERT INTO join_block (`uid`, `bid`, `is_approved`, `approve_num`)VALUES (".$this->uid.",".$this->bid.",1,0);";
+		}
+		else if ($data_block[0]["cn"] !== "0" && !$data_user[0]["cn"]){
+			//non-empty block but new user
+
+
+			$j_block="INSERT INTO join_block (`uid`, `bid`, `is_approved`, `approve_num`)VALUES (".$this->uid.",".$this->bid.",0,0);";
+
+		}
+		else{
+			throw new Exception("already exist the user!");
+		}
+
+
+		$data = $conn->query($j_block);
+
+		if ($data==1) {
+			return $data;
+		} else {
+			throw new Exception("error, cannot join the block!");
+		}
+
+	}
+		public function getJson() {
+		try {
+			$this->res['data'] = $this->doExecute();
+		} catch (Exception $e) {
+			$this->res['status'] = -1;
+			$this->res['message'] = $e->getMessage();
+		} finally {
+			echo json_encode($this->res);
+		}
+	}
+}
+$joinblock = new joinBlock;
+$data = $joinblock->getJson();
+?>
