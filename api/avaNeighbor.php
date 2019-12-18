17972cbed866f2d6aacb3501b959818d828f6810
fthongstar <haochz3@uci.edu>
2019-12-18 03:21:55 -0500

    update no neigh

--- api/avaNeighbor.php
+++ api/avaNeighbor.php
@@ -10,10 +10,10 @@ class avaNeighbor extends api {
 	public function doExecute() {
 		// in case of sql injection
 
-		// $uid = intval($_GET['uid']);
-		$uid = intval($_COOKIE['uid']);
+		$uid = intval($_GET['uid']);
+		// $uid = intval($_COOKIE['uid']);
 		
-		$sql = 'SELECT u.uid, u.firstname, u.lastname, jb.bid FROM user u, join_block jb,(SELECT uid, bid FROM join_block WHERE uid=' . $uid . ') as t WHERE u.uid = jb.uid AND jb.bid = t.bid AND is_approved=1 AND u.uid<>' . $uid . ' AND u.uid NOT IN (SELECT neighbor_uid FROM neighbor WHERE uid=' . $uid . ' AND is_valid=1);';
+		$sql = 'SELECT u.uid, u.firstname, u.lastname, jb.bid FROM user u, join_block jb,(SELECT uid, bid FROM join_block WHERE uid=' . $uid . ' AND is_approved=1) as t WHERE u.uid = jb.uid AND jb.bid = t.bid AND is_approved=1 AND u.uid<>' . $uid . ' AND u.uid NOT IN (SELECT neighbor_uid FROM neighbor WHERE uid=' . $uid . ' AND is_valid=1);';
 
 
 		// var_dump($sql);
