c29aa10342b3a01aa2135477abacadb456b1ed9e
fthongstar <haochz3@uci.edu>
2019-12-18 19:59:53 -0500

    final final submit

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,18 +1,18 @@
-
+ 
 -- sign up (create an account) --> insert a value in user table
 -- update personal information --> update the user table
 INSERT INTO USER (`firstname`, `lastname`, `user_pwd`, `state`, `city`, `zipcode`, `address`, `phone_num`, `photo`, `self_intro`, `family_intro`, `profile_timestamp`, `email`) VALUES 
-('Jia', 'Shi', 'psw', 'New York', 'Brooklyn', 11201,'111 Street', 9491118218,'jia.jpg', null, null, '2019-11-01 13:10:11', 'js11182@nyu.edu'),
-('Haochen','Zhou', 'nice111','New York', 'Brooklyn',11201,'123 Street', 1314411011, null, null, null, '2019-11-02 09:10:11', 'hz2204@nyu.edu'),
-('Mary','Brown', '233','New York', 'Brooklyn',11201,'132 Street', 2123228202, null, null, null,'2019-11-20 09:10:11','mb@nyu.edu'),
-('Lily','Brown', 'ni121e','New York', 'Brooklyn',11201,'121 Street', 333333333, null, null, 'Mary Brown is my mother','2019-11-21 09:10:,11', 'lb1@nyu.edu'),
-('Lisa','Zhou', '2hdsk','New York', 'Brooklyn',11211,'40 Avenue', 323433033, null, null, null,'2019-12-02 09:10:,11', 'lz@nyu.edu'),
-('Jack','Lee', '23ls1','New York', 'Brooklyn',11211,'41 Avenue', 333139303, null, null, null,'2019-11-28 09:10:,11', 'jl@nyu.edu'),
-('Muimui','Choing','jdk13','New York', 'Brooklyn',11201,'199 Street',3248923891,null,null,null,'2019-12-01 09:10:,11','mc@nyu.edu'),
-('Linda','Frader','1jdwkn','New York', 'New York',10011,'14 Street',2188923111,null,null,null,'2019-11-01 09:10:,11','lf@nyu.edu'),
-('Amy','Kaynor','22k313','New York', 'New York',10011,'15 Street',2188920890,null,null,null,'2019-11-11 09:10:,11','ak@nyu.edu'),
-('Tony','Loring','den0d','New York', 'New York',10012,'16 Street',2188923201,null,null,null,'2019-11-21 09:10:,11','tl@nyu.edu'),
-('David','Wayne','pwkj10','New York', 'New York',10012,'17 Street',2188003811,null,null,null,'2019-12-01 09:10:,11','dw@nyu.edu');
+('Jia', 'Shi', 'e10adc3949ba59abbe56e057f20f883e', 'New York', 'Brooklyn', 11201,'111 Street', 9491118218,'jia.jpg', null, null, '2019-11-01 13:10:11', 'js11182@nyu.edu'),
+('Haochen','Zhou', 'e10adc3949ba59abbe56e057f20f883e','New York', 'Brooklyn',11201,'123 Street', 1314411011, null, null, null, '2019-11-02 09:10:11', 'hz2204@nyu.edu'),
+('Mary','Brown', 'e10adc3949ba59abbe56e057f20f883e','New York', 'Brooklyn',11201,'132 Street', 2123228202, null, null, null,'2019-11-20 09:10:11','mb@nyu.edu'),
+('Lily','Brown', 'e10adc3949ba59abbe56e057f20f883e','New York', 'Brooklyn',11201,'121 Street', 333333333, null, null, 'Mary Brown is my mother','2019-11-21 09:10:,11', 'lb1@nyu.edu'),
+('Lisa','Zhou', 'e10adc3949ba59abbe56e057f20f883e','New York', 'Brooklyn',11211,'40 Avenue', 323433033, null, null, null,'2019-12-02 09:10:,11', 'lz@nyu.edu'),
+('Jack','Lee', 'e10adc3949ba59abbe56e057f20f883e','New York', 'Brooklyn',11211,'41 Avenue', 333139303, null, null, null,'2019-11-28 09:10:,11', 'jl@nyu.edu'),
+('Muimui','Choing','e10adc3949ba59abbe56e057f20f883e','New York', 'Brooklyn',11201,'199 Street',3248923891,null,null,null,'2019-12-01 09:10:,11','mc@nyu.edu'),
+('Linda','Frader','e10adc3949ba59abbe56e057f20f883e','New York', 'New York',10011,'14 Street',2188923111,null,null,null,'2019-11-01 09:10:,11','lf@nyu.edu'),
+('Amy','Kaynor','e10adc3949ba59abbe56e057f20f883e','New York', 'New York',10011,'15 Street',2188920890,null,null,null,'2019-11-11 09:10:,11','ak@nyu.edu'),
+('Tony','Loring','e10adc3949ba59abbe56e057f20f883e','New York', 'New York',10012,'16 Street',2188923201,null,null,null,'2019-11-21 09:10:,11','tl@nyu.edu'),
+('David','Wayne','e10adc3949ba59abbe56e057f20f883e','New York', 'New York',10012,'17 Street',2188003811,null,null,null,'2019-12-01 09:10:,11','dw@nyu.edu');
 
 -- e.g. 
 -- A adds B as a neighbor: insert both values of A and B. 
@@ -55,7 +55,7 @@ INSERT INTO join_block(`uid`, `bid`, `is_approved`, `approve_num`) VALUES
 (1, 1, 1, 0),
 (2, 1, 1, 1),
 (3, 1, 1, 2),
-(4, 1, 1, 3),
+(4, 1, -1, 3),
 (5, 2, 1, 0),
 (6, 2, 1, 1),
 (7, 1, 1, 3),
